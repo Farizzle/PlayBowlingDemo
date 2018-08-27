d834dc9eb4dd61ac7e3839e854eead80097d1715
Faris <faris.zaman@metricell.com>
2018-08-27 15:14:23 +0100

    Initial Commit

---
+++ PlayBowling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayBowling
+//
+//  Created by Faris Zaman on 8/27/18.
+//  Copyright © 2018 Faris Zaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
